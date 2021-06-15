87cec46b0b7b540089882fa1691b90fe50b68003
rospino74 <34315725+rospino74@users.noreply.github.com>
2021-06-15 15:18:28 +0200

    Aggiunto target a Java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,12 +39,11 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
 
+java.targetCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
 
